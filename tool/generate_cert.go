410ff3d49e1f83cfcd9b971cb85121fd09f63547
rfc <reuben.fletchercostin@gmail.com>
2022-05-20 07:36:00 +1000

    tool - generate_cert - common name, out path flags

--- tool/generate_cert.go
+++ tool/generate_cert.go
@@ -23,18 +23,22 @@ import (
 	"math/big"
 	"net"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
 
 var (
-	host       = flag.String("host", "", "Comma-separated hostnames and IPs to generate a certificate for")
-	validFrom  = flag.String("start-date", "", "Creation date formatted as Jan 1 15:04:05 2011")
-	validFor   = flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for")
-	isCA       = flag.Bool("ca", false, "whether this cert should be its own Certificate Authority")
-	rsaBits    = flag.Int("rsa-bits", 2048, "Size of RSA key to generate. Ignored if --ecdsa-curve is set")
-	ecdsaCurve = flag.String("ecdsa-curve", "", "ECDSA curve to use to generate a key. Valid values are P224, P256 (recommended), P384, P521")
-	ed25519Key = flag.Bool("ed25519", false, "Generate an Ed25519 key")
+	host        = flag.String("host", "", "Comma-separated hostnames and IPs to generate a certificate for")
+	commonName  = flag.String("common-name", "", "value for certificate subject common name")
+	validFrom   = flag.String("start-date", "", "Creation date formatted as Jan 1 15:04:05 2011")
+	validFor    = flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for")
+	isCA        = flag.Bool("ca", false, "whether this cert should be its own Certificate Authority")
+	rsaBits     = flag.Int("rsa-bits", 2048, "Size of RSA key to generate. Ignored if --ecdsa-curve is set")
+	ecdsaCurve  = flag.String("ecdsa-curve", "", "ECDSA curve to use to generate a key. Valid values are P224, P256 (recommended), P384, P521")
+	ed25519Key  = flag.Bool("ed25519", false, "Generate an Ed25519 key")
+	outKeyPath  = flag.String("out-key", "key.pem", "output path for key file")
+	outCertPath = flag.String("out-cert", "cert.pem", "output path for cert file")
 )
 
 func publicKey(priv any) any {
@@ -109,13 +113,18 @@ func main() {
 		log.Fatalf("Failed to generate serial number: %v", err)
 	}
 
+	subject := pkix.Name{
+		Organization: []string{"Acme Co"},
+	}
+	if *commonName != "" {
+		subject.CommonName = *commonName
+	}
+
 	template := x509.Certificate{
 		SerialNumber: serialNumber,
-		Subject: pkix.Name{
-			Organization: []string{"Acme Co"},
-		},
-		NotBefore: notBefore,
-		NotAfter:  notAfter,
+		Subject:      subject,
+		NotBefore:    notBefore,
+		NotAfter:     notAfter,
 
 		KeyUsage:              keyUsage,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
@@ -141,21 +150,30 @@ func main() {
 		log.Fatalf("Failed to create certificate: %v", err)
 	}
 
-	certOut, err := os.Create("cert.pem")
+	certPath, err := filepath.Abs(*outCertPath)
 	if err != nil {
-		log.Fatalf("Failed to open cert.pem for writing: %v", err)
+		log.Fatalf("Invalid cert path %s: %v", *outCertPath, err)
+	}
+	keyPath, err := filepath.Abs(*outKeyPath)
+	if err != nil {
+		log.Fatalf("Failed key path %s: %v", *outKeyPath, err)
+	}
+
+	certOut, err := os.Create(certPath)
+	if err != nil {
+		log.Fatalf("Failed to open %s for writing: %v", certPath, err)
 	}
 	if err := pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes}); err != nil {
-		log.Fatalf("Failed to write data to cert.pem: %v", err)
+		log.Fatalf("Failed to write data to %s: %v", certPath, err)
 	}
 	if err := certOut.Close(); err != nil {
-		log.Fatalf("Error closing cert.pem: %v", err)
+		log.Fatalf("Error closing %s: %v", certPath, err)
 	}
-	log.Print("wrote cert.pem\n")
+	log.Printf("wrote %s\n", certPath)
 
-	keyOut, err := os.OpenFile("key.pem", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	keyOut, err := os.OpenFile(keyPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		log.Fatalf("Failed to open key.pem for writing: %v", err)
+		log.Fatalf("Failed to open %s for writing: %v", keyPath, err)
 		return
 	}
 	privBytes, err := x509.MarshalPKCS8PrivateKey(priv)
@@ -163,10 +181,10 @@ func main() {
 		log.Fatalf("Unable to marshal private key: %v", err)
 	}
 	if err := pem.Encode(keyOut, &pem.Block{Type: "PRIVATE KEY", Bytes: privBytes}); err != nil {
-		log.Fatalf("Failed to write data to key.pem: %v", err)
+		log.Fatalf("Failed to write data to %s: %v", keyPath, err)
 	}
 	if err := keyOut.Close(); err != nil {
-		log.Fatalf("Error closing key.pem: %v", err)
+		log.Fatalf("Error closing %s: %v", keyPath, err)
 	}
-	log.Print("wrote key.pem\n")
+	log.Printf("wrote %s\n", keyPath)
 }
